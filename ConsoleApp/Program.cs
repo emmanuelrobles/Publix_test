c2a08c2abd2c7370ef785cb14c7104c3896f3ae1
Emmanuel <emmanuelrobles94@gmail.com>
2022-06-23 14:31:28 -0400

    added console app

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Application.Interfaces;
+using Application.Services;
+
+// creates a service
+var service = new ApplicationService(new Settings { DemeritPoints = 1, SpeedDelta = 5 });
+
+try
+{
+    Console.Write("Speed Limit:");
+    var speedLimit = int.Parse(Console.ReadLine() ?? string.Empty);
+    Console.Write("Driver Speed:");
+    var driverSpeed = int.Parse(Console.ReadLine() ?? string.Empty);
+
+    Console.WriteLine($"Demerit Points: {service.GetDemerit(driverSpeed,speedLimit)}");
+}
+catch (InvalidCastException)
+{
+    Console.WriteLine("Cant cast to an int :|");
+}
+
+
+
+class Settings : ISettings
+{
+    public uint SpeedDelta { get; init; }
+    public uint DemeritPoints { get; init; }
+}
